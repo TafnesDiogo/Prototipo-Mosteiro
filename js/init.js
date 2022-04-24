4243eb4c5e98889d791fedc0a6e74c33e802511d
Táfnes Diogo <tafnes.diogo@gmail.com>
2022-04-24 16:41:15 -0300

    Arquivos

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.dropdown-trigger').dropdown();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
